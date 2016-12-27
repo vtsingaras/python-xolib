b734934974c96246bba5dadac3843ee9713ef8f0
Vyronas Tsingaras <vyronas@vtsingaras.me>
2016-12-27 16:55:55 +0200

    Release v0.2.2 to fix import error.

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
     name='xolib',
-    version='0.2.1',
+    version='0.2.2',
     author='Vyronas Tsingaras',
     author_email='vyronas@vtsingaras.me',
     packages=['xolib'],
     url='https://github.com/vtsingaras/python-xolib',
-    download_url='https://github.com/vtsingaras/python-xolib/archive/v0.2.1.tar.gz',
+    download_url='https://github.com/vtsingaras/python-xolib/archive/v0.2.2.tar.gz',
     license='LICENSE.txt',
     description='Helper library for interfacing with xo-server.',
     long_description=open('README.rst').read(),
